58f77a4baa8ee31e44c315303d40bb0fb2649d3f
louca859 <louca859@student.liu.se>
2014-10-08 15:13:39 +0200

    created correct level buttons (the flat ones), renamed them

--- states/Preloader.js
+++ states/Preloader.js
@@ -12,10 +12,10 @@ ClickIt.Preloader.prototype = {
     this.load.setPreloadSprite(this.preloadBar);
     this.load.image('logo', 'assets/ClickIt_with_dots.png');
 
-    this.load.spritesheet('startButton', 'assets/startButton.png', 240, 80);
-    this.load.spritesheet('levelOneImage', 'assets/levelOneButton.png', 240, 80);
-    this.load.spritesheet('levelTwoImage', 'assets/levelTwoButton.png', 240, 80);
-    this.load.spritesheet('levelThreeImage', 'assets/levelThreeButton.png', 240, 80);
+    this.load.spritesheet('startButton', 'assets/level_button_play.png', 240, 80);
+    this.load.spritesheet('levelOneImage', 'assets/level_button_one.png', 240, 80);
+    this.load.spritesheet('levelTwoImage', 'assets/level_button_two.png', 240, 80);
+    this.load.spritesheet('levelThreeImage', 'assets/level_button_three.png', 240, 80);
   },
 
   create: function() {
